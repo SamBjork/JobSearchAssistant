898e87791dd8460932ae5294b62c403f6fe59d53
Samuel Björkqvist <samuel.bjorkqvist@gmail.com>
2021-06-03 14:58:14 +0200

    Identity scaffolded successfully

---
+++ JobSearchAssistant/Server/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using JobSearchAssistant.Server.Data;
+using JobSearchAssistant.Server.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(JobSearchAssistant.Server.Areas.Identity.IdentityHostingStartup))]
+namespace JobSearchAssistant.Server.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
